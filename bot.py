92ba470c8e9736ee8956fce6e4a5a4b43e072a22
saidkamol <167999812+SaidkamolA@users.noreply.github.com>
2025-05-16 22:32:28 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Text
-from aiogram.types import Message
 import asyncio
 
 API_TOKEN = "7549706885:AAGRJuC317xNQHuV3A_s_i5ohruXVtNMOS0"
@@ -8,9 +6,10 @@ API_TOKEN = "7549706885:AAGRJuC317xNQHuV3A_s_i5ohruXVtNMOS0"
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
-@dp.message(Text(equals="obervorlaring", ignore_case=True))
-async def respond_roll(message: Message):
-    await message.answer("🧻")
+@dp.message()
+async def respond_roll(message: types.Message):
+    if message.text and message.text.lower() == "obervorlaring":
+        await message.answer("🧻")
 
 async def main():
     await dp.start_polling(bot)
