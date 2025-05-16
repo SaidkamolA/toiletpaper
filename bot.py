4243b730cc03342fd2413b60b1da8324944f6a2f
saidkamol <167999812+SaidkamolA@users.noreply.github.com>
2025-05-16 22:33:53 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,17 +1,23 @@
+import logging
 from aiogram import Bot, Dispatcher, types
 import asyncio
 
 API_TOKEN = "7549706885:AAGRJuC317xNQHuV3A_s_i5ohruXVtNMOS0"
+
+logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher()
 
 @dp.message()
 async def respond_roll(message: types.Message):
+    logging.info(f"Получено сообщение: {message.text}")
     if message.text and message.text.lower() == "obervorlaring":
         await message.answer("🧻")
+        logging.info("Ответил эмодзи 🧻")
 
 async def main():
+    logging.info("Запуск бота...")
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
